f89c7d214539da155a114699412bf67cb07909db
luan goedert <luan_goedert2013@hotmail.com>
2020-10-07 17:48:56 -0300

    shared preferences

--- app/src/main/java/com/example/tasks/service/repository/PersonRepository.kt
+++ app/src/main/java/com/example/tasks/service/repository/PersonRepository.kt
@@ -2,30 +2,38 @@ package com.example.tasks.service.repository
 
 import com.example.convidados.ui.Service.Repository.remote.RetrofitClient
 import com.example.tasks.service.HeaderModel
+import com.example.tasks.service.constants.TaskConstants
+import com.example.tasks.service.listener.APIListener
 import com.example.tasks.service.repository.remote.PersonService
+import com.google.gson.Gson
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
 
 class PersonRepository {
 
-   private val mRemote = RetrofitClient.createService(PersonService::class.java)
+    private val mRemote = RetrofitClient.createService(PersonService::class.java)
 
-    fun login(email: String, password: String) {
+    fun login(email: String, password: String, listener: APIListener) {
 
         val call: Call<HeaderModel> = mRemote.login(email, password)
         //remote.login(email, password)
 
         //Async
-        call.enqueue(object: Callback<HeaderModel> {
+        call.enqueue(object : Callback<HeaderModel> {
             override fun onResponse(call: Call<HeaderModel>, response: Response<HeaderModel>) {
-                val header = response.body()
+                if (response.code() != TaskConstants.HTTP.SUCCESS) {
+                    val validation =
+                        Gson().fromJson(response.errorBody()!!.string(), String()::class.java)
+                    listener.onFailure(validation)
+                } else {
+                    response.body()?.let { listener.onSuccess(it) }
+                }
             }
 
             override fun onFailure(call: Call<HeaderModel>, t: Throwable) {
-                val s = ""
+                listener.onFailure(t.message.toString())
             }
-
         })
 
 
--- app/src/main/java/com/example/tasks/view/LoginActivity.kt
+++ app/src/main/java/com/example/tasks/view/LoginActivity.kt
@@ -1,9 +1,14 @@
 package com.example.tasks.view
 
+import android.app.ProgressDialog.show
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.Toast
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import com.example.tasks.R
 import com.example.tasks.viewmodel.LoginViewModel
@@ -12,6 +17,8 @@ import kotlinx.android.synthetic.main.activity_login.*
 class LoginActivity : AppCompatActivity(), View.OnClickListener {
 
     private lateinit var mViewModel: LoginViewModel
+
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -53,7 +60,16 @@ class LoginActivity : AppCompatActivity(), View.OnClickListener {
     /**
      * Observa ViewModel
      */
-    private fun observe() {}
+    private fun observe() {
+        mViewModel.login.observe(this, Observer {
+            if(it) {
+                startActivity(Intent(this, MainActivity::class.java))
+            }
+            else {
+                Toast.makeText(this, "Erro no Login", Toast.LENGTH_SHORT).show()
+            }
+        })
+    }
 
     /**
      * Autentica usuário
